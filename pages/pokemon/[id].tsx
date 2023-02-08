0d46d7c6edfd661c6f99022d9bf35617830ef763
Juan <gurquiza8@gmail.com>
2023-02-07 22:20:41 -0600

    Incremental Static Regeneration

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -116,7 +116,7 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
             }
         ],
         */
-        fallback: false
+        fallback: 'blocking'
     }
 }
 
@@ -124,10 +124,22 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   
     const { id } = params as { id: string };
 
+    const pokemon = await getPokemonInfo( id );
+
+    if ( !pokemon ) {
+        return {
+            redirect: {
+                destination: '/',
+                permanent: false
+            }
+        }
+    }
+
     return {
         props: {
-            pokemon: await getPokemonInfo( id )
-        }
+            pokemon
+        },
+        revalidate: 86400 // Segundos (24 horas)
     }
   
 }
